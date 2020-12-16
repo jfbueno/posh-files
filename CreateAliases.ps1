8ab762be19574b7099272dc9a6cd3a349ac0138e
jfbueno <8502026+jfbueno@users.noreply.github.com>
2020-12-15 21:37:19 -0300

    Alias para git push --force-with-lease

--- CreateAliases.ps1
+++ CreateAliases.ps1
@@ -6,6 +6,7 @@ Add-Alias fetch 'git fetch'
 Add-Alias push 'git push'
 Add-Alias pull 'git pull'
 Add-Alias pushsync 'git push --set-upstream origin HEAD'
+Add-Alias fush 'git push --force-with-lease'
 
 function glog {
 	git log --oneline -$args
