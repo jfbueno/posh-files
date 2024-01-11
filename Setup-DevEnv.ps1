2c0e94c795964df611dd0aca341559d30a9c702b
jfbueno <8502026+jfbueno@users.noreply.github.com>
2024-01-10 23:03:58 -0300

    Novos apps padrões

---
+++ Setup-DevEnv.ps1
@@ -0,0 +1,9 @@
+winget install -e --id Microsoft.DotNet.SDK.7
+
+winget install -e --id Microsoft.VisualStudioCode
+
+winget install -e --id Microsoft.VisualStudio.2022.Community
+
+winget install -e --id LINQPad.LINQPad.6
+
+# install vs-code
