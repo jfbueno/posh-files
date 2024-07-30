ee4177811e4ca239f68cee1f42d3f5a471e2d907
jfbueno <8502026+jfbueno@users.noreply.github.com>
2024-07-30 16:30:05 -0300

    Separação das instalações em outro script

--- Install.ps1
+++ Install.ps1
@@ -1,4 +1,14 @@
 winget install JanDeDobbeleer.OhMyPosh --source winget
 winget install bat --source winget
 
+Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+Install-Module posh-cli -Scope CurrentUser    
+
+Install-TabCompletion
+
+Install-Module posh-dotnet
+Install-Module DockerCompletion
+Install-Module PSKubectlCompletion
+
 # TODO: install nerd-font
+# Trocar as cores do terminal
