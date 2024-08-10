e24009b6b03414b7109cee9850ed0a578ea91e12
jfbueno <buenojeferson@outlook.com>
2024-08-10 14:20:40 -0300

    Altera versões das ferramentas de dev

--- Setup-DevEnv.ps1
+++ Setup-DevEnv.ps1
@@ -1,9 +1,7 @@
-winget install -e --id Microsoft.DotNet.SDK.7
+winget install -e --id Microsoft.DotNet.SDK.8
 
 winget install -e --id Microsoft.VisualStudioCode
 
 winget install -e --id Microsoft.VisualStudio.2022.Community
 
-winget install -e --id LINQPad.LINQPad.6
-
-# install vs-code
+winget install -e --id LINQPad.LINQPad.8
