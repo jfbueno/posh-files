b76f2db348238074af12a6eeb05408b73cc4f72f
jfbueno <buenojeferson@outlook.com>
2024-08-10 00:00:54 -0300

    Instalação de ferramentas fundamentais

--- Install.ps1
+++ Install.ps1
@@ -1,14 +1,14 @@
+winget install -e --id Microsoft.WindowsTerminal
 winget install JanDeDobbeleer.OhMyPosh --source winget
 winget install bat --source winget
+winget install dandavison.delta --source winget
+winget install notepad++ --source winget
 
 Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-Install-Module posh-cli -Scope CurrentUser    
+Install-Module posh-cli -Scope CurrentUser
 
 Install-TabCompletion
 
 Install-Module posh-dotnet
 Install-Module DockerCompletion
 Install-Module PSKubectlCompletion
-
-# TODO: install nerd-font
-# Trocar as cores do terminal
