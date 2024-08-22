294c258a471b467789c7913ef21ef5273f6f1386
jfbueno <buenojeferson@outlook.com>
2024-08-22 11:02:48 -0300

    Instalação do powertoys

--- Install.ps1
+++ Install.ps1
@@ -3,6 +3,7 @@ winget install JanDeDobbeleer.OhMyPosh --source winget
 winget install bat --source winget
 winget install dandavison.delta --source winget
 winget install notepad++ --source winget
+winget install --id Microsoft.PowerToys --source winget
 
 Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
 Install-Module posh-cli -Scope CurrentUser
