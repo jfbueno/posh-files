ac666b3d7bc4cc2c5d65ed098e30dd45b08059fe
jfbueno <8502026+jfbueno@users.noreply.github.com>
2025-05-29 11:24:26 -0300

    Move aliases do git para .gitconfig

--- CreateAliases.ps1
+++ CreateAliases.ps1
@@ -1,25 +1,5 @@
-Add-Alias co 'git checkout'
-Add-Alias gst 'git status'
-Add-Alias grb 'git rebase'
-Add-Alias glg 'git log --oneline -10'
-Add-Alias fetch 'git fetch'
-Add-Alias push 'git push'
-Add-Alias pull 'git pull'
-Add-Alias pushsync 'git push --set-upstream origin HEAD'
-Add-Alias fush 'git push --force-with-lease'
+Add-Alias touch 'New-Item -ItemType File'
 
 function glog {
 	git log --oneline -$args
-}
-
-function cg-th() {
-	Change-Theme
-}
-
-<#
-    .Synopsis
-    Altera para um tema do OMP menos poluído 
-#>
-function Change-Theme(){
-	(@(& 'C:/Users/bueno/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\bueno\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' --print) -join "`n") | Invoke-Expression
 }
